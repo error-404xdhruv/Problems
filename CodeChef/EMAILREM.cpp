45d0228878c04881a105e9def0398b4fe739cd64
Dhruv <dhruv.nitj@gmail.com>
2022-04-21 09:54:09 +0530

    apr21

---
+++ CodeChef/EMAILREM.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std ;
+
+int main(int argc, char const *argv[])
+{
+    int N, U ;
+    cin >> N >> U;
+    cout << N-U <<endl ;
+
+    return 0;
+}
