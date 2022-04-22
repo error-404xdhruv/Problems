39a12ad581515184e7c8881323f5a64d602fdba2
Dhruv <dhruv.nitj@gmail.com>
2022-04-22 13:29:33 +0530

    apr22

--- LeetCode/1207.cpp
+++ LeetCode/1207.cpp
@@ -1,10 +1,9 @@
 
 // https://leetcode.com/problems/unique-number-of-occurrences/
 #include <iostream>
-#include <cstdio>
 using namespace std ;
 
-int main(int argc, char const *argv[])
+int main()
 {
     
 
