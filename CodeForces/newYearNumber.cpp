a8a7fc0ddaf8c3af396a61967c96048f3615759f
Dhruv <dhruv.nitj@gmail.com>
2022-04-23 12:05:45 +0530

    apr23

---
+++ CodeForces/newYearNumber.cpp
@@ -0,0 +1,27 @@
+
+// https://codeforces.com/problemset/problem/1475/B
+#include <iostream>
+using namespace std ;
+void doWork()
+{
+    int n;
+    cin >> n;
+    if (n%2020<= n/2020)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+int main()
+{
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        doWork();
+    }
+    return 0;
+}
