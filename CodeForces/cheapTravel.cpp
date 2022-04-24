5bbf877b117b88d48024b0536779a76419a2ea19
Dhruv <dhruv.nitj@gmail.com>
2022-04-24 10:06:51 +0530

    apr24

---
+++ CodeForces/cheapTravel.cpp
@@ -0,0 +1,20 @@
+
+// https://codeforces.com/problemset/problem/466/A
+
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+    int n , m , a, b;
+    cin >> n >> m >> a >> b ;
+    if (m*a <= b)
+    {
+        cout << n*a << endl;
+    }
+    else
+    {
+        cout << (n/m)*b+min((n%m)*a,b) << endl ;
+    }
+    return 0;
+}
