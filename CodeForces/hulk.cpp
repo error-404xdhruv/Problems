4671ca1cab78bc8f2d2057b6648968d40d10d2af
Dhruv <dhruv.nitj@gmail.com>
2022-04-25 14:17:31 +0530

    apr25

---
+++ CodeForces/hulk.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    int n ;
+    cin >> n ;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == n)
+        {
+            if ((i&1)==1)
+            {
+                cout << "I hate it" << endl ;
+            }
+            else
+            {
+                cout << "I love it" << endl ;
+            }            
+        }
+        else
+        {
+            if ((i&1)==1)
+            {
+                cout << "I hate that " ;
+            }
+            else
+            {
+                cout << "I love that " ;
+            }
+        }
+    }
+    return 0;
+}
