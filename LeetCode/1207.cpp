f84db0bac07a766b98e1baf3d3780ab55bef94b2
Dhruv <dhruv.nitj@gmail.com>
2022-04-26 22:16:18 +0530

    apr26

--- LeetCode/1207.cpp
+++ LeetCode/1207.cpp
@@ -4,41 +4,46 @@
 
 class Solution {
 public:
-    bool uniqueOccurrences(vector<int>& arr) {
-        vector<int> ans;
-        // Loop to check number of occurrences
-        for(int i=0; i<arr.size(); i++)
+    bool uniqueOccurrences(vector<int>&arr) {
+        vector <int> ans;
+        //counting number of occurences
+        for (int i = 0; i < arr.size(); i++)
         {
-            int temp = arr[i]; // Store the value of arr[i] in a variable so after traversing through second loop value of arr[i] doesn't change in between
-            int count = 0; // Counting occurences
-            
-            if(temp != INT_MIN)
+            int x = arr[i] ;
+            int count = 0;
+            if(x != INT_MIN)
             {
-                for(int j=0; j<arr.size(); j++)
+                for (int j = 0; j<arr.size(); j++)
+            {
+                if(x == arr[j])
                 {
-                    if(temp == arr[j])
-                    {
-                        count++;
-                        arr[j] = INT_MIN; // Initializing visited numbers of array with INT_MIN
-                    }
+                    count++ ;
+                    arr[j] = INT_MIN ;
                 }
-                if(count>0)
-                    ans.push_back(count);
             }
-        }
-        // Loop to check frequency of a number
-        for(int i=0; i<ans.size(); i++)
-        {
-            int count_fr = 0; // Counting frequency of a number in dynamic array
-            for(int j=0; j<ans.size(); j++)
+            if(count > 0)
             {
-                if(ans[i] == ans[j])
-                    count_fr++;
+                ans.push_back (count) ;
             }
-            if(count_fr > 1)
-                return false;
+            }
         }
-        return true; 
         
+        //counting frequency
+        for (int i = 0; i < ans.size() ; i++)
+        {
+            int count1 = 0;
+            for (int j = 0 ; j < ans.size() ; j++)
+            {
+                if (ans[i]==ans[j])
+                {
+                    count1++ ;
+                }
+            }
+            if(count1 > 1)
+            {
+                return false ;
+            }
+        }
+        return true ;
     }
 };
