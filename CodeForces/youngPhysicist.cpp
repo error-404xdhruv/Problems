03cd22765f1b55aa6f06b2612eb2d9eaff97e8b3
Dhruv <dhruv.nitj@gmail.com>
2022-04-26 23:01:15 +0530

    apr26

---
+++ CodeForces/youngPhysicist.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    int t;
+    cin >> t ;
+    int array[100][3];
+    for (int i = 0; i < t; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            cin >> array[i][j] ;
+        }
+        
+    }
+    int sum = 0 ;
+    for (int j = 1; j <= 3; j++)
+    {
+        for (int i = 0; i < t; i++)
+        {
+            sum = sum + array[i][j] ;
+        }
+        if (sum != 0)
+        {
+            cout << "NO" << endl ;
+            goto end ;
+        }
+    }
+    cout << "YES" << endl ;
+
+    end:
+    return 0;
+}
