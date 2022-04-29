aee3cc7954c63cf509d3998e31a466e5565d4e2b
Dhruv <dhruv.nitj@gmail.com>
2022-04-29 16:16:15 +0530

    apr29

---
+++ CodeForces/queueSchool.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    string q;
+    int n, t;
+    cin >> n >> t ;
+    cin >> q ;
+    for (int i = 0; i < t; i++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (q[i]=='B'&&q[i+1]=='G')
+            {
+                swap(q[i], q[i+1]) ;
+                i++ ;
+            }
+        }
+    }
+    cout << q << endl;
+
+    return 0;
+}
