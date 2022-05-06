9779cc5d3fa4dd31744ce875526a9cd9101c57e9
Dhruv <dhruv.nitj@gmail.com>
2022-05-06 18:41:07 +0530

    mY6

---
+++ CodeForces/A_Dubstep.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    string in;
+    cin >> in ;
+    int temp = 1;
+    for (int i = 0; i < in.length(); i++)
+    {
+        if (in[i]=='W'&&in[i+1]=='U'&&in[i+2]=='B')
+        {
+            i+=2 ;
+            if (temp==0)
+            {
+                cout << " " ;
+            }
+            temp = 1 ;
+            continue;
+        }
+        else
+        {
+            cout << in[i] ;
+            temp =0 ;
+        }
+        
+        
+    }
+    
+
+    return 0;
+}
