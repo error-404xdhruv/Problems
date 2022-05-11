ad17fe97a3248a0324a045d7a54bb1bebf072a80
Dhruv <dhruv.nitj@gmail.com>
2022-05-11 22:38:52 +0530

    may11

--- Good_Pairs.cpp
+++ Good_Pairs.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
 using namespace std ;
 
-// long int doWork(long int arr1[], long int arr2[], long int n)
-// {
-//     long int count =0 ;
-//     for (long int j = 0; j < n; j++)
-//     {
-//         for (long int i = 0; i < j; i++)
-//         {
-//             if (arr1[i]==arr2[j] || arr1[j]==arr2[i])
-//             {
-//                 count++ ;
-//             }
-            
-//         }
+long int doWork(long int arr1[], long int arr2[], long int n)
+{
+    long int count =0 ;
+    for (long int j = 0; j < n; j++)
+    {
+        for (long int i = 0; i < j; i++)
+        {
+            if (arr1[i]==arr2[j] || arr1[j]==arr2[i])
+            {
+                count++ ;
+            }
+        }
         
-//     }
-//     return count ;
-// }
+    }
+    return count ;
+}
 
 int main ()
 {
     long int t ;
     cin >> t ;
-    // while (t--)
-    // {
-    //     long int n;
-    //     long int arr1[100000] , arr2[100000];
-    //     for (long int i = 0; i < n; i++)
-    //     {
-    //         cin >> arr1[i] ;
-    //     }
-    //     for (long int i = 0; i < n; i++)
-    //     {
-    //         cin >> arr2[i] ;
-    //     }
-    //     long int ans = doWork(arr1, arr2, n) ;
-    //     cout << ans << endl ;
-    // }
+    
+    while (t--)
+    {
+        long int n; 
+        long int arr1[100000] , arr2[100000];
+        for (long int i = 0; i < n; i++)
+        {
+            cin >> arr1[i] ;
+        } 
+        for (long int i = 0; i < n; i++)
+        {
+            cin >> arr2[i] ;
+        }
+        long int ans = doWork(arr1, arr2, n) ;
+        cout << ans << endl ;
+    }
     
     return 0 ;
 }
