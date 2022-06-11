416b651d1500c05c478c945b72bd65b9e07d3818
Dhruv <dhruv.nitj@gmail.com>
2022-06-11 17:00:01 +0530

    jun11

---
+++ CodeForces/A_Fancy_Fence.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define l long long
+#define vl vector<long long>
+#define vi vector<int>
+#define vp vector<vector<int>>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a ;
+        if (360 % (180 - a) == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
