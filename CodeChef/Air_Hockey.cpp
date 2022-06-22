2ce04ec92da7cde81ba7536edb7b4b549206e787
Dhruv <dhruv.nitj@gmail.com>
2022-06-22 22:23:18 +0530

    jun22

---
+++ CodeChef/Air_Hockey.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define l long long
+#define vl vector<long long>
+#define vi vector<int>
+#define v2 vector<vector<int>>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << 7 - max(a, b) << endl;
+    }
+
+    return 0;
+}
